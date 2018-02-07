318e1942fd1d6b31b570f023366fb0963dd91244
Kaan Goksal <kaangoksal@gmail.com>
2018-02-07 17:30:30 -0500

    nearly completed the db models for SQLite

--- dbModels/db_Models.py
+++ dbModels/db_Models.py
@@ -1,12 +1,67 @@
+"""
+Author: Kaan Goksal
+Date: Sometime around January 2018
+Summary: This file stores the table structures for the client, accesspoint, clientsessions and so on. Nothing complex
+just lots of busy work
+"""
+
+
 from sqlalchemy import Column, VARCHAR, TEXT, TIMESTAMP, func, Date, Integer
-from dbModels.declerations import Base
+from declerations import Base, engine, session
+
+
+class Accesspoint_dbModel(Base):
+    __tablename__ = 'access_points'
+
+    access_point_bssid = Column(VARCHAR(30), primary_key=True)
+    first_seen = Column(Date, server_default=func.now())
+    tag = Column(TEXT) # Desing issue, we might have multiple tags, this undermines the search efficiency!
+
+    def __init__(self, bssid):
+        self.access_point_bssid = bssid
+
+    @staticmethod
+    def get_access_point(bssid):
+        """
+        Returns the access point from the db
+        :param bssid: the mac address of the accesspoint
+        :return: none if not registered, the database object if found in the database
+        """
+        fetched_access_point = session.query(Accesspoint_dbModel).filter_by(access_point_bssid=bssid).first()
+        return fetched_access_point
+
+
+class WifiClient_dbModel(Base):
+    """
+    This class is just for storing and keeping a record of wifi clients
+    """
+    __tablename__ = 'wifi_clients'
+
+    client_mac = Column(VARCHAR(30), primary_key=True)
+    first_seen = Column(Date, server_default=func.now())
+    tag = Column(TEXT)
+
+    def __init__(self, client_mac):
+        self.client_mac = client_mac
+
+    @staticmethod
+    def get_client(client_mac):
+        """
+        This function returns the client object or null if it doesn't exist
+        :param client_mac:
+        :return:
+        """
+        db_client = session.query(WifiClient_dbModel).filter_by(client_mac=client_mac).first()
+        return db_client
 
 #TODO change indexing for connected access_points and other aux tables
 
-class WifiClientSession_dbModel(Base):
-    __tablename__ = 'wifi_client_sessions'
 
-    client_mac = Column(VARCHAR(30), primary_key=True)
+class WifiClientSession_dbModel(Base):
+
+    __tablename__ = 'wifi_client_sessions'
+    id = Column(Integer, primary_key=True)
+    client_mac = Column(VARCHAR(30), nullable=False)
     session_start = Column(Date, nullable=False, server_default=func.now())
     session_end = Column(Date)
     connected_access_points = Column(VARCHAR(40), nullable=False) # this is tied to another table
@@ -51,7 +106,6 @@ class WifiClientSession_connected_access_points(Base):
         pass #TODO
 
 
-
 class WifiClientSession_spotted_channels(Base):
     __tablename__ = 'client_spotted_channels'
 
@@ -77,7 +131,7 @@ class WifiClientSession_spotted_channels(Base):
 
 
 class WifiClientSession_observed_rssi(Base):
-    __tablename__ = 'client_spotted_channels'
+    __tablename__ = 'observed_rssi'
 
     id = Column(Integer, primary_key=True, nullable=False)
     session_key = Column(VARCHAR(60), nullable=False)
@@ -97,6 +151,8 @@ class WifiClientSession_observed_rssi(Base):
         :param session_key: session key of the session
         :return: a list of rssi objects?
         """
-        pass  # TODO implement
+        pass # TODO implement
 
+
+Base.metadata.create_all(bind=engine)
 
